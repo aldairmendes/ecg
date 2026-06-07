addf3a34cef6875a71a6470e560dea1f3372a931
Thales <thaleslucas36@gmail.com>
2026-06-07 12:07:08 -0300

    test: atualiza testes e dependências do projeto

--- include/EcgBuffer.h
+++ include/EcgBuffer.h
@@ -1,14 +1,19 @@
-#ifndef ECG_DATA_H
-#define ECG_DATA_H
+#ifndef ECG_BUFFER_H
+#define ECG_BUFFER_H
 
 #include <stdint.h>
 
 #define BUFFER_SIZE 187
 
 struct ECG_Buffer {
-    float samples[BUFFER_SIZE];
-    uint16_t index;
+    uint16_t samples[BUFFER_SIZE];
+    uint8_t index;
     bool ready;
 };
+
+void ecg_buffer_init(ECG_Buffer* buf);
+bool ecg_buffer_push(ECG_Buffer* buf, uint16_t sample);
+void ecg_buffer_reset(ECG_Buffer* buf);
+bool ecg_buffer_is_ready(const ECG_Buffer* buf);
 
 #endif
